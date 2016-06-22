ce9119af0c3199bb3c5841910d6f3aafc5c45cfc
Thomas Smith <thomasm2@cisco.com>
2016-06-21 18:27:03 -0700

    Changed arg order. Fixed int parse bug. - Also implemented testmod. - Fixed some docs
    Signed-off-by: Thomas Smith <thomasm2@cisco.com>

--- dbg.py
+++ dbg.py
@@ -28,15 +28,33 @@ class Dbg(object):
     General debugging facility class
 
     usage example:
-    d = Dbg(stderr=True, verbosity_floor=3)
+
+    >>> from dbg import Dbg
+    >>> d = Dbg(name="Test Log", verbosity_floor=3, stdout=True, stderr=False)
+    >>> d.dbg(3, "I am a debug statement")
+    DEBUG Test Log 2016-06-21 17:35:46,170 from: <module> I am a debug statement
+    >>> d.warning(1, "I am a warning statement")
+    WARNING Test Log 2016-06-21 17:36:01,078 from: <module> I am a warning statement
+    >>> d.dbg(4, "I am a debug statement who will never be seen because the verbosity floor is too low :(")
+
+    >>> from dbg import Dbg
+    >>> d = Dbg(name="Test Log", verbosity_floor=3, stdout=True, stderr=False)
+    >>> d.dbg(4, "I am a debug statement who will never be seen because the verbosity floor is too low :(", 12, 3)
+    >>> d.dbg(1, "test", 1, 2, 3)
+    DEBUG Test Log 2016-06-21 18:25:42,228 from: <module> test 1 2 3
     '''
 
-    def __init__(self, name, stdout=False, stderr=True, syslog=False, tofile='', customh=None, verbosity_floor=0):
+    def __init__(self, name=__name__, verbosity_floor=0, stdout=False, stderr=True, syslog=False,
+                 tofile='', customh=None, print_level=True):
+
         'Debug initializer'
 
         LINUX_LOG_DEV = '/dev/log'
 
-        fmt = logging.Formatter('%(levelname)s %(name)s %(asctime)s %(message)s')
+        if print_level:
+            fmt = logging.Formatter('%(levelname)s %(name)s %(asctime)s %(message)s')
+        else:
+            fmt = logging.Formatter('%(name)s %(asctime)s %(message)s')
         lvl = logging.DEBUG
 
         # emit only debug messages below the verbosity floor level
@@ -85,6 +103,7 @@ class Dbg(object):
             fhdlr.setFormatter(fmt)
             self.log_op.addHandler(fhdlr)
 
+        # user can specify a custom hander
         if customh:
             ch = customh
             ch.setLevel(lvl)
@@ -96,7 +115,8 @@ class Dbg(object):
 
         # print a debug message only if permitted by the verbosity floor level
         if level <= self.verbosity_floor:
-            self.log_op.log(severity, 'from: {cn} {mg} {ag}'.format(cn=caller_name, mg=message, ag=' '.join(args)))
+            self.log_op.log(severity, 'from: {cn} {mg} {ag}'.format(cn=caller_name, mg=message, ag=' '.join(
+                map(lambda ag: str(ag), args))))
 
     def dbg(self, level, message, *args):
         'Log a debugging message if our verbosity floor is high enough'
@@ -104,7 +124,7 @@ class Dbg(object):
         # obtain the name of our caller as a string
         caller_name = sys._getframe().f_back.f_code.co_name
 
-        self.__log(caller_name, 10, level, message, *args)
+        self.__log(caller_name, logging.DEBUG, level, message, *args)
 
     def info(self, level, message, *args):
         'Log an info message if our verbosity floor is high enough'
@@ -112,7 +132,7 @@ class Dbg(object):
         # obtain the name of our caller as a string
         caller_name = sys._getframe().f_back.f_code.co_name
 
-        self.__log(caller_name, 20, level, message, *args)
+        self.__log(caller_name, logging.INFO, level, message, *args)
 
     def warning(self, level, message, *args):
         'Log a warning message if our verbosity floor is high enough'
@@ -120,7 +140,7 @@ class Dbg(object):
         # obtain the name of our caller as a string
         caller_name = sys._getframe().f_back.f_code.co_name
 
-        self.__log(caller_name, 30, level, message, *args)
+        self.__log(caller_name, logging.WARNING, level, message, *args)
 
     def error(self, level, message, *args):
         'Log an error message if our verbosity floor is high enough'
@@ -128,7 +148,7 @@ class Dbg(object):
         # obtain the name of our caller as a string
         caller_name = sys._getframe().f_back.f_code.co_name
 
-        self.__log(caller_name, 40, level, message, *args)
+        self.__log(caller_name, logging.ERROR, level, message, *args)
 
     def critical(self, level, message, *args):
         'Log a critical message if our verbosity floor is high enough'
@@ -136,7 +156,7 @@ class Dbg(object):
         # obtain the name of our caller as a string
         caller_name = sys._getframe().f_back.f_code.co_name
 
-        self.__log(caller_name, 50, level, message, *args)
+        self.__log(caller_name, logging.critical, level, message, *args)
 
     def close_handers(self):
         for handler in self.handlers:
@@ -152,13 +172,7 @@ class Dbg(object):
 if __name__ == '__main__':
     # setup debugging facility
 
-    with Dbg(__name__, stderr=True, syslog=True, tofile='/tmp/log.log', verbosity_floor=3) as d:
-        # emit a debug message above the floor
-        d.dbg(2, 'This level 2 message should appear because the',
-                  'verbosity floor = ' + str(d.verbosity_floor))
+    import doctest
 
-        # this debug message should not appear
-        d.error(4, 'This level 4 message should NOT appear because the',
-                  'verbosity floor = ' + str(d.verbosity_floor))
-
+    doctest.testmod()
 
